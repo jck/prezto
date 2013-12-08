7ec3dff6c933031a340efc67145c68d1087f322d
Keerthan Jaic <jckeerthan@gmail.com>
2013-12-08 06:11:28 -0500

    autoenv

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -31,15 +31,20 @@ if (( ! $+commands[python] && ! $+commands[pyenv] )); then
   return 1
 fi
 
+local venv_script=virtualenvwrapper
+if zstyle -T ':prezto:module:python' lazy_venv; then
+    venv_script+=_lazy
+fi
+
 # Load virtualenvwrapper into the shell session.
-if (( $+commands[virtualenvwrapper_lazy.sh] )); then
+if (( $+commands[$venv_script.sh] )); then
   # Set the directory where virtual environments are stored.
   export WORKON_HOME="$HOME/.virtualenvs"
 
   # Disable the virtualenv prompt.
   VIRTUAL_ENV_DISABLE_PROMPT=1
 
-  source "$commands[virtualenvwrapper_lazy.sh]"
+  source "$commands[$venv_script.sh]"
 fi
 
 #
